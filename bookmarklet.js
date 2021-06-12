6e1713a79dc214104277e2225fa52de57f9604aa
Johnathon Selstad <makeshifted@gmail.com>
2021-06-11 21:36:08 -0700

    Move the delay...

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,6 +1,10 @@
 // Add this as your bookmarklet:
 // javascript:{var s=document.createElement("script");s.src="https://zalo.github.io/Holopladertoy/bookmarklet.js",document.body.appendChild(s);};void(0);
 
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
 // Execute on a delay in-case the body hasn't been constructed yet...
 setTimeout(function() {
   // Don't initialize the bookmarklet again if there's already a StatusView
@@ -12,6 +16,8 @@ setTimeout(function() {
       'https://zalo.github.io/Holopladertoy/index.js'];
     //var i, s, ss = ['/Holoplay.min.js', '/index.js']; // The local testing version...
     for (i = 0; i != ss.length; i++) {
+      if (i === 1)
+        await sleep(2000);
       s = document.createElement('script');
       s.src = ss[i];
       document.body.appendChild(s);
--- index.js
+++ index.js
@@ -1,30 +1,28 @@
 class Holopladertoy {
     constructor() {
-        setTimeout(() => {
-            this.calibrationData = HoloPlay.Calibration.getCalibration();
+        this.calibrationData = HoloPlay.Calibration.getCalibration();
 
-            console.log(this.calibrationData);
+        console.log(this.calibrationData);
 
-            const screenInches =
-                this.calibrationData.screenW.value / this.calibrationData.DPI.value;
-            let newPitch = this.calibrationData.pitch.value * screenInches;
+        const screenInches =
+            this.calibrationData.screenW.value / this.calibrationData.DPI.value;
+        let newPitch = this.calibrationData.pitch.value * screenInches;
 
-            // account for tilt in measuring pitch horizontally
-            newPitch *= Math.cos(Math.atan(1.0 / this.calibrationData.slope.value));
-            quiltMaterial.uniforms.pitch.value = newPitch;
+        // account for tilt in measuring pitch horizontally
+        newPitch *= Math.cos(Math.atan(1.0 / this.calibrationData.slope.value));
+        quiltMaterial.uniforms.pitch.value = newPitch;
 
-            // tilt
-            let newTilt = this.calibrationData.screenH.value /
-                (this.calibrationData.screenW.value * this.calibrationData.slope.value);
-            if (this.calibrationData.flipImageX.value == 1) {
-                newTilt *= -1;
-            }
+        // tilt
+        let newTilt = this.calibrationData.screenH.value /
+            (this.calibrationData.screenW.value * this.calibrationData.slope.value);
+        if (this.calibrationData.flipImageX.value == 1) {
+            newTilt *= -1;
+        }
 
-            console.log(newPitch, newTilt);
+        console.log(newPitch, newTilt);
 
-            // TODO: Replace the variables in the Shadertoy script with these new values
-            // Maybe also trigger a cheeky shader recompile for an instant preview
-        }, 2000);
+        // TODO: Replace the variables in the Shadertoy script with these new values
+        // Maybe also trigger a cheeky shader recompile for an instant preview
     }
 }
 
