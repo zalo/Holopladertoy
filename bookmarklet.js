7c1319020e39a6d83538a3c25ebe5db18c4973ab
Johnathon Selstad <makeshifted@gmail.com>
2021-06-11 21:37:50 -0700

    Mark async

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,12 +1,8 @@
 // Add this as your bookmarklet:
 // javascript:{var s=document.createElement("script");s.src="https://zalo.github.io/Holopladertoy/bookmarklet.js",document.body.appendChild(s);};void(0);
 
-function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
-}
-
 // Execute on a delay in-case the body hasn't been constructed yet...
-setTimeout(function() {
+setTimeout(async function() {
   // Don't initialize the bookmarklet again if there's already a StatusView
   var existingStatusView = document.getElementById("statusView");
   if(typeof(existingStatusView) === 'undefined' || existingStatusView === null){
@@ -17,7 +13,7 @@ setTimeout(function() {
     //var i, s, ss = ['/Holoplay.min.js', '/index.js']; // The local testing version...
     for (i = 0; i != ss.length; i++) {
       if (i === 1)
-        await sleep(2000);
+        await new Promise(r => setTimeout(r, 2000));
       s = document.createElement('script');
       s.src = ss[i];
       document.body.appendChild(s);
